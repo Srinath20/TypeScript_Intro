14d135538841052a45e5b48bef37937f5755a547
Srinath20 <srinathgollapinni>
2024-08-25 22:51:37 +0530

    Used few typescript features to optimize code and restructured the project

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -1,36 +1,36 @@
-import { Router } from 'express';
-import { Todo } from '../models/todo';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
 /*import express from 'express';
 const router = express.Router(); */
-let todos: Todo[] = [];
-const router = Router();
-
+let todos = [];
+const router = (0, express_1.Router)();
 router.get('/', (req, res, next) => {
     return res.status(200).json({ todos: todos });
-})
-
+});
 router.post('/todo', (req, res, next) => {
-    const newTodo: Todo = {
+    const body = req.body;
+    const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo);
     return res.status(201).json({ message: 'Added todo', todos: todos });
 });
-
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: 'Updated todo ', todos: todos });
     }
     return res.status(404).json({ message: 'Could not find the todo with mentioned id' });
 });
-
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    const params = req.params;
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
     return res.status(200).json({ message: 'Deleted the todo successfully' });
 });
-
-export default router;
+exports.default = router;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,32 +1,43 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
+import { Router } from 'express';
+import { Todo } from '../models/todo';
 /*import express from 'express';
 const router = express.Router(); */
-let todos = [];
-const router = (0, express_1.Router)();
+let todos: Todo[] = [];
+const router = Router();
+
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 router.get('/', (req, res, next) => {
     return res.status(200).json({ todos: todos });
-});
-router.post('/todo', (req, res, next) => {
-    const newTodo = {
+})
+
+router.post('/todo', (req, res, next) => { //passed callback function is a controller
+    const body = req.body as RequestBody;
+    const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo);
     return res.status(201).json({ message: 'Added todo', todos: todos });
 });
+
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params as RequestParams;
+    const tid = params.todoId;
+    const body = req.body as RequestBody;
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: 'Updated todo ', todos: todos });
     }
     return res.status(404).json({ message: 'Could not find the todo with mentioned id' });
 });
+
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    const params = req.params as RequestParams;
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
     return res.status(200).json({ message: 'Deleted the todo successfully' });
 });
-exports.default = router;
+
+export default router;
